9599470b06b6da08a0a9f4c997d20c36278be691
mansoorraufi <mansoorraufi786@gmail.com>
2022-09-28 20:09:32 -0400

    First commit

---
+++ src/FinelProject/FinelClass.java
@@ -0,0 +1,13 @@
+package FinelProject;
+
+public class FinelClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("git step one");
+	
+	
+	}
+
+}
